9c618b0ce5fe893ec0bf35ec77a1800700e62b04
kalyan327 <114553995+kalyan327@users.noreply.github.com>
2023-05-02 10:23:48 +0530

    Create intro_to_size.c
    Here is the basic code for knowing the size of an operator.

---
+++ 3. Pointers Arithmetic & “sizeof” operator/intro_to_size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("size of int = %d\n",sizeof(num));
+
+    double grade = 99.8;
+
+    printf("Size of int = %d\n",sizeof(grade));
+}
